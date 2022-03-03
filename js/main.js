5ca9a221b84e091b94a1f2793e8c996f9c4f4d73
lum1neuz <lum1neuz@yahoo.com>
2022-03-03 22:44:57 +0800

    added new homepage

--- js/main.js
+++ js/main.js
@@ -21,3 +21,51 @@ window.onscroll = () => {
  scrollTop.style.opacity = 0;
  }
 };
+
+
+//swiper
+  var autoplay = 5000;
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  slidesPerView: 'auto',
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      touchRatio: 1,
+    },
+    // when window width is >= 640px
+    960: {
+      touchRatio: 0,
+    }
+  }
+});
+
+var galleryTop = new Swiper('.gallery-top', {
+  effect: 'fade',
+  controller: {
+    control: galleryThumbs
+  },
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+  autoplay: {
+    delay: autoplay,
+    disableOnInteraction: false
+  },
+  on: {
+    init: function () {
+      $(".swiper-progress-bar").removeClass("animate");
+      $(".swiper-progress-bar").removeClass("active");
+      $(".swiper-progress-bar").eq(0).addClass("animate");
+      $(".swiper-progress-bar").eq(0).addClass("active");
+    },
+    slideChangeTransitionStart: function () {
+      $(".swiper-progress-bar").removeClass("animate");
+      $(".swiper-progress-bar").removeClass("active");
+      $(".swiper-progress-bar").eq(0).addClass("active");
+    },
+    slideChangeTransitionEnd: function () {
+      $(".swiper-progress-bar").eq(0).addClass("animate");
+    }
+  }
+});
