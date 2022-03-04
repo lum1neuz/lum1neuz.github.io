189db6723baf3c7023322e9a81b10d1e4f790cf6
lum1neuz <lum1neuz@yahoo.com>
2022-03-04 17:43:33 +0800

    index final version

--- js/main.js
+++ js/main.js
@@ -69,3 +69,19 @@ var galleryTop = new Swiper('.gallery-top', {
     }
   }
 });
+
+
+
+// check scrollbar width and offsets from header nav
+
+// Create the measurement node
+var scrollDiv = document.createElement("div");
+scrollDiv.className = "scrollbar-measure";
+document.body.appendChild(scrollDiv);
+
+// Get the scrollbar width
+var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+console.log(scrollbarWidth); // Mac:  15
+
+// Delete the DIV 
+document.body.removeChild(scrollDiv);
